5bee93121101bebcc4474fd9693ffbede9bfbdf3
websiteriesen <office@websiteriesen.at>
2024-09-17 16:52:37 +0200

    Refactoring

--- src/app/shared/Interfaces/Course.ts
+++ src/app/shared/Interfaces/Course.ts
@@ -3,16 +3,13 @@ import { EventLocation } from "./EventLocation";
 export interface Course {
     id: string;
     name: string;
-    dates: [],
-    instructor: string,
-    eventLocationId: number
-  }
-
-  export interface CourseResponse {
-    id: string;
-    name: string;
-    dates: [],
+    dates: Date[],
     instructor: string,
     eventLocationId: number,
     eventLocation: EventLocation
+  }
+
+  interface Date {
+    begin: string,
+    end: string
   }
--- src/app/shared/backend.service.ts
+++ src/app/shared/backend.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { StoreService } from './store.service';
-import { CourseResponse } from './Interfaces/Course';
+import { Course } from './Interfaces/Course';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +11,7 @@ export class BackendService {
   constructor(private http: HttpClient, private storeService: StoreService) { }
 
   public getCourses() {
-    this.http.get<CourseResponse[]>('http://localhost:5000/courses?_expand=eventLocation').subscribe(data => {
+    this.http.get<Course[]>('http://localhost:5000/courses?_expand=eventLocation').subscribe(data => {
       this.storeService.courses = data;
     });
   }
